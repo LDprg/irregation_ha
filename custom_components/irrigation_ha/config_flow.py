e90da421c225a1b8201298d61389c27246a93bbf
LDprg <lukas_4dr@gmx.at>
2024-05-21 14:45:20 +0000

    fix config flow

--- custom_components/irrigation_ha/config_flow.py
+++ custom_components/irrigation_ha/config_flow.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
+from homeassistant.components.binary_sensor import (
+    DOMAIN as BINARY_SENSOR_DOMAIN,
+)
+from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.config_entries import ConfigFlow
-from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.const import CONF_COUNT
 from homeassistant.const import CONF_ENTITY_ID
 
@@ -18,7 +21,7 @@ class IrrigationHaFlow(ConfigFlow, domain=irri.DOMAIN):
 
     async def async_step_user(
             self, _user_input,
-    ) -> ConfigFlowResult:
+    ):
         """
         Init step
         """
@@ -27,6 +30,9 @@ class IrrigationHaFlow(ConfigFlow, domain=irri.DOMAIN):
             step_id='user',
             data_schema=vol.Schema({
                 vol.Required(CONF_COUNT, default=1): cv.positive_int,
-                vol.Required(CONF_ENTITY_ID): cv.positive_int,
+                vol.Required(CONF_ENTITY_ID): vol.Any(
+                    cv.entity_domain(BINARY_SENSOR_DOMAIN),
+                    cv.entity_domain(SWITCH_DOMAIN),
+                ),
             }),
         )
