16a9f8720fad4d4b0913a35ecea97ed8f4ac4c0e
LDprg <lukas_4dr@gmx.at>
2024-06-22 18:50:51 +0200

    fix default value setting

--- custom_components/irrigation_ha/switch.py
+++ custom_components/irrigation_ha/switch.py
@@ -7,6 +7,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.const import (
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
+)
 
 from . import const as irri
 from .coordinator import IRRICoordinator
@@ -25,7 +30,7 @@ async def async_setup_entry(
     )
 
 
-class IRRISwitch(CoordinatorEntity, SwitchEntity):
+class IRRISwitch(CoordinatorEntity, SwitchEntity, RestoreEntity):
     """Representation of a Sensor."""
 
     def __init__(self, coordinator, uid):
@@ -36,6 +41,15 @@ class IRRISwitch(CoordinatorEntity, SwitchEntity):
         self._attr_unique_id = uid
 
         self._attr_device_class = SwitchDeviceClass.SWITCH
+
+    async def async_added_to_hass(self) -> None:
+        """Restore last state."""
+        await super().async_added_to_hass()
+        if (last_state := await self.async_get_last_state()) is not None:
+            if last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+                self._attr_is_on = bool(last_state.state)
+            elif last_state.state is not STATE_UNKNOWN:
+                self._attr_is_on = True
 
     async def async_turn_on(self, **kwargs):
         """Turn the entity on."""
