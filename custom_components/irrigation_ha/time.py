0d4e7b0067b5ffc9eeeee383ed688d5312f0f2d2
LDprg <lukas_4dr@gmx.at>
2024-06-22 18:16:20 +0200

    fix restore of time

--- custom_components/irrigation_ha/time.py
+++ custom_components/irrigation_ha/time.py
@@ -1,57 +1,20 @@
 """Platform for sensor integration."""
 
 from __future__ import annotations
-import dataclasses
 from datetime import time
-from typing import Any, Self
+from typing import Final
 
 from homeassistant.components.time import TimeEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.helpers.restore_state import ExtraStoredData, RestoreEntity
+from homeassistant.helpers.restore_state import RestoreEntity
 
 from . import const as irri
 from .coordinator import IRRICoordinator
 
-
-@dataclasses.dataclass
-class TimeExtraStoredData(ExtraStoredData):
-    """Object to hold extra stored data."""
-
-    native_value: time | None
-
-    def as_dict(self) -> dict[str, Any]:
-        """Return a dict representation of the number data."""
-        return dataclasses.asdict(self)
-
-    @classmethod
-    def from_dict(cls, restored: dict[str, Any]) -> Self | None:
-        """Initialize a stored number state from a dict."""
-        try:
-            return cls(
-                restored["native_value"],
-            )
-        except KeyError:
-            return None
-
-
-class RestoreTime(TimeEntity, RestoreEntity):
-    """Mixin class for restoring previous number state."""
-
-    @property
-    def extra_restore_state_data(self) -> TimeExtraStoredData:
-        """Return number specific state data to be restored."""
-        return TimeExtraStoredData(
-            self.native_value,
-        )
-
-    async def async_get_last_number_data(self) -> TimeExtraStoredData | None:
-        """Restore native_*."""
-        if (restored_last_extra_data := await self.async_get_last_extra_data()) is None:
-            return None
-        return TimeExtraStoredData.from_dict(restored_last_extra_data.as_dict())
+_TIME_TRANSLATION_FORMAT: Final = "%H:%M:%S"
 
 
 async def async_setup_entry(
@@ -67,7 +30,7 @@ async def async_setup_entry(
     )
 
 
-class IRRITime(CoordinatorEntity, RestoreTime):
+class IRRITime(CoordinatorEntity, TimeEntity, RestoreEntity):
     """Representation of a Sensor."""
 
     def __init__(self, coordinator, uid):
@@ -80,13 +43,17 @@ class IRRITime(CoordinatorEntity, RestoreTime):
         self._attr_native_value = None
 
     async def async_added_to_hass(self) -> None:
-        """When entity is added to Home Assistant."""
+        """Restore last state."""
         await super().async_added_to_hass()
-        last_number_data = await self.async_get_last_number_data()
-        if (last_number_data is not None) and (
-            last_number_data.native_value is not None
-        ):
-            await self.async_set_value(last_number_data.native_value)
+        if (
+            last_state := await self.async_get_last_state()
+        ) is not None and last_state.state is not None:
+            await self.async_set_value(
+                time.strptime(
+                    last_state.state,
+                    _TIME_TRANSLATION_FORMAT,
+                ),
+            )
 
     async def async_set_value(self, value: time) -> None:
         """Update the current value."""
